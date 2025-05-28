29a9f2487a21dab8656d3cbacb83fc97ef6ac382
Aurel Hamon <hamon.aurel@lilo.org>
2025-05-28 17:56:16 +0200

    checking correlation between AA and ModRef analysis

--- llvm/lib/Transforms/Utils/AliasTestPass.cpp
+++ llvm/lib/Transforms/Utils/AliasTestPass.cpp
@@ -9,12 +9,14 @@
 #include "llvm/Transforms/Utils/AliasTestPass.h"
 #include "llvm/ADT/SetVector.h"
 #include "llvm/Analysis/AliasAnalysis.h"
+#include "llvm/Analysis/MemoryLocation.h"
 #include "llvm/IR/BasicBlock.h"
 #include "llvm/IR/Function.h"
 #include "llvm/IR/Instruction.h"
 #include "llvm/IR/Instructions.h"
 #include "llvm/Pass.h"
 #include "llvm/Support/Casting.h"
+#include "llvm/Support/ModRef.h"
 #include "llvm/Support/raw_ostream.h"
 
 using namespace llvm;
@@ -42,12 +44,19 @@ PreservedAnalyses AliasTestPass::run(Function &F,
   AliasAnalysis &AA = AM.getResult<AAManager>(F);
 
   errs() << F.getName() << " contains the following load and store : \n";
+  for (Value *Load : LdStTables.Loads) errs() << *Load << "\n";
+  for (Value *Store : LdStTables.Stores) errs() << *Store << "\n";
 
   // declaring counts for differents answers of alias analysis
   int countMayAlias = 0;
   int countNotAlias = 0;
   int countMustAlias = 0;
   int countPartialAlias = 0;
+
+  int countMod = 0;
+  int countRef = 0;
+  int countModRef = 0;
+  int countNoModRef = 0;
   
   for (Value *Load : LdStTables.Loads) {
     for (Value *Store : LdStTables.Stores) {
@@ -59,14 +68,31 @@ PreservedAnalyses AliasTestPass::run(Function &F,
         case AliasResult::NoAlias:      countNotAlias++;     break;
         case AliasResult::PartialAlias: countPartialAlias++; break;
       }
-      errs() << "  " << AR << ": " << *Load << " <-> " << *Store << '\n';
+
+      ModRefInfo StoreLocModRef = AA.getModRefInfo(dyn_cast<LoadInst>(Load), 
+        MemoryLocation::get(dyn_cast<StoreInst>(Store)));
+
+      ModRefInfo LoadLocModRef = AA.getModRefInfo(dyn_cast<StoreInst>(Store), 
+        MemoryLocation::get(dyn_cast<LoadInst>(Load)));
+
+      std::string MemDep = (
+        LoadLocModRef == ModRefInfo::NoModRef && StoreLocModRef == ModRefInfo::NoModRef
+      ) ? "NO" : "THERE IS A";
+
+      errs() << "  " << AR << " ;"
+             << MemDep << " mem-dep between : "
+             << *Load << " <-> " << *Store << '\n';
     }
   }
-
 
   errs() << "Count of may alias : " << countMayAlias << "\n";
   errs() << "Count of no alias : " << countNotAlias << "\n";
   errs() << "Count of must alias : " << countMustAlias << "\n";
   errs() << "Count of partial alias : " << countPartialAlias << "\n";
+
+  errs() << "Count of mod : " << countMod << "\n";
+  errs() << "Count of ref : " << countRef << "\n";
+  errs() << "Count of modref : " << countModRef << "\n";
+  errs() << "Count of nomodref : " << countNoModRef << "\n";
   return PreservedAnalyses::all();
 }
